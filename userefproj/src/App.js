59f2964caec4f6a6af30f26ad4314a13e63cb68a
JayantaMondalATPL <30437092+Jack070@users.noreply.github.com>
2022-04-22 11:16:01 +0530

    useRefFinal

--- userefproj/src/App.js
+++ userefproj/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
+import UseRefBasics from "./useRefSetup/1-useRef-basics";
 function App() {
   return (
-    <div className='container'>
-      <h2>Advanced Tutorial</h2>
+    <div className="container">
+      <UseRefBasics />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
