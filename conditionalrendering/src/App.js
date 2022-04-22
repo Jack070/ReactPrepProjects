b845aef896a06f40222d9541b43665ee1aea8afa
JayantaMondalATPL <30437092+Jack070@users.noreply.github.com>
2022-04-22 08:03:51 +0530

    Conditional Rendering Final

--- conditionalrendering/src/App.js
+++ conditionalrendering/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from "react";
+import ShowHide from "./conditionalRenderingSetup/3-show-hide";
 function App() {
   return (
-    <div className='container'>
-      <h2>Advanced Tutorial</h2>
+    <div className="container">
+      <h2>
+        <ShowHide />
+      </h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- conditionalrendering/src/conditionalRenderingSetup/2-short-circuit.js
+++ conditionalrendering/src/conditionalRenderingSetup/2-short-circuit.js
@@ -1,12 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
+  // const firstValue = text || "Hello World";
+  // const secondValue = text && "Hello World";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>value : {secondValue}</h1> */}
+      <h1>{text || "Jay Kumar"}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        Toggle Error
+      </button>
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>There is an error...</p>
+      ) : (
+        <div>
+          <h2>There is no error...</h2>
+        </div>
+      )}
+      {/* {text && <h1>Hello World</h1>}
+      {!text && <h1>Hello World Again</h1>} */}
+    </>
+  );
 };
 
 export default ShortCircuit;
