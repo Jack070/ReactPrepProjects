aef09632d0eb6b107e3c7626b6939ab74dbbb1ca
JayantaMondalATPL <30437092+Jack070@users.noreply.github.com>
2022-04-23 09:31:42 +0530

    PropdrillingFinal

--- propdrilling/src/App.js
+++ propdrilling/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
+import PropDrilling from "./propdrillingSetup/1-prop-drilling";
 function App() {
   return (
-    <div className='container'>
-      <h2>Advanced Tutorial</h2>
+    <div className="container">
+      <PropDrilling />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- propdrilling/src/propdrillingSetup/1-prop-drilling.js
+++ propdrilling/src/propdrillingSetup/1-prop-drilling.js
@@ -1,10 +1,46 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+import { data } from "../data";
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [people, setPeople] = useState(data);
+  const removePerson = (id) => {
+    setPeople((prevValue) => {
+      return prevValue.filter((person) => person.id !== id);
+    });
+  };
+  return (
+    <section>
+      <h3>Prop Drilling</h3>
+      <List removePerson={removePerson} people={people} />
+    </section>
+  );
+};
+
+const List = ({ people, removePerson }) => {
+  return (
+    <>
+      {people.map((person) => {
+        return (
+          <SinglePerson
+            key={person.id}
+            {...person}
+            removePerson={removePerson}
+          />
+        );
+      })}
+    </>
+  );
+};
+
+const SinglePerson = ({ id, name, removePerson }) => {
+  return (
+    <div className="item">
+      <h4>{name}</h4>
+      <button onClick={() => removePerson(id)}>Remove</button>
+    </div>
+  );
 };
 
 export default PropDrilling;
